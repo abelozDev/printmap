6bb5c2bc4c38cd4248df5f5f4451d76c33714e5d
Artem Kolganov <102026263+kolgan35@users.noreply.github.com>
2025-01-27 18:43:01 +0300

    inc version

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         minSdk = 24
-        version = "1.0.1"
+        version = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
