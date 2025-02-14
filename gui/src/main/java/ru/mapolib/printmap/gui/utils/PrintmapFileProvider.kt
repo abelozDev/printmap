be84a844c70c2f852b1cbabeb7138d554817fb95
Artem Kolganov <102026263+kolgan35@users.noreply.github.com>
2025-02-14 15:24:43 +0300

    fix file provider

---
+++ gui/src/main/java/ru/mapolib/printmap/gui/utils/PrintmapFileProvider.kt
@@ -0,0 +1,6 @@
+package ru.mapolib.printmap.gui.utils
+
+import androidx.core.content.FileProvider
+
+class PrintmapFileProvider: FileProvider() {
+}
--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -40,8 +40,9 @@ android {
 }
 
 dependencies {
-    implementation("com.github.abelozDev.printmap:lib:1.0.6")
-    implementation("com.github.abelozDev.printmap:gui:1.0.6")
+    implementation(project(":gui"))
+    /*implementation("com.github.abelozDev.printmap:lib:1.0.6")
+    implementation("com.github.abelozDev.printmap:gui:1.0.6")*/
     implementation(project(":gui"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
